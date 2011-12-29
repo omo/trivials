e9964c77d44958e867362964ed6daa0114a1b29f
Hajime Morrita <omo@dodgson.org>
2011-12-29 22:23:41 +0900

    Wrote triv-octopress-new-post

---
+++ elisp/trivials.el
@@ -0,0 +1,19 @@
+
+(defcustom triv-octopress-workdir (expand-file-name "~/work/pumpkinsugar")
+  "An Octopress checkout which Emacs tries to step in")
+
+(defun triv-octopress-new-post (title)
+  (interactive "MTitle: ")
+  (let ((command-str (format "bash -l -c 'source $HOME/.rvm/scripts/rvm && rvm use 1.9.2 > /dev/null && cd %s && rake new_post[\"%s\"]'" 
+			    triv-octopress-workdir title))
+	(command-result) (flename))
+    (progn
+      (message command-str)
+      (setq command-result (shell-command-to-string command-str))
+      (setq filename (concat triv-octopress-workdir "/" 
+			     (replace-regexp-in-string "\\(Creating new post: \\)\\|\n" "" command-result)))
+      (find-file filename)
+      )))
+(define-key ctl-x-map "J" 'triv-octopress-new-post)
+
+(provide 'trivials)

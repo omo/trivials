1fbaa9254dbdfdd348386fe8fb87b9aebadbf6a0
Hajime Morrita <omo@dodgson.org>
2012-05-12 17:00:56 +0900

    updated

--- elisp/trivials.el
+++ elisp/trivials.el
@@ -7,12 +7,14 @@
   (let ((command-str (format "bash -l -c 'source $HOME/.rvm/scripts/rvm && rvm use 1.9.2 > /dev/null && cd %s && rake new_post[\"%s\"]'" 
 			    triv-octopress-workdir title))
 	(command-result) (filename))
-    (progn
-      (message command-str)
-      (setq command-result (shell-command-to-string command-str))
-      (setq filename (concat triv-octopress-workdir "/" 
-			     (replace-regexp-in-string "\\(Creating new post: \\)\\|\n" "" command-result)))
-      (find-file filename)
-      )))
+    (message command-str)
+    (setq command-result (shell-command-to-string command-str))
+    (setq filename (concat triv-octopress-workdir "/" 
+			   (replace-regexp-in-string "\\(Creating new post: \\)\\|\n" "" command-result)))
+    (find-file filename)))
+
+(defun triv-octopress-show-dir ()
+  (interactive)
+  (find-file (concat triv-octopress-workdir "/source/_posts/")))
 
 (provide 'trivials)
---
+++ pushanki.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd ~/.ankit.d && git add . && git commit -m "updated" && git push
+
